3dbcfb8ce48c070a1bb3b4863c565c8fa46030f7
Snowman2409 <harshveera01@gmail.com>
2024-06-12 18:24:41 +0000

    veux: Adapt to Cherish OS

--- cherish_veux.mk
+++ cherish_veux.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common crDroid stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Cherish  stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit some common device props
-TARGET_USES_AOSP_RECOVERY := true
+BUILD_WITH_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_SUPPORTS_CALL_RECORDING := true
@@ -26,11 +26,20 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_ENABLE_BLUR := true
 TARGET_EXCLUDES_AUDIOFX := true
 
+CHERISH_BUILD_TYPE := Unofficial
+WITH_GMS := true
+
+# Cherish props
+CHERISH_MAINTAINER := Bad_Snomwman
+CHERISH_CHIPSET := SM6375
+CHERISH_BATTERY := 5000mAh
+CHERISH_DISPLAY := 1080x2400
+
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2201116SG
-PRODUCT_NAME := lineage_veux
+PRODUCT_NAME := cherish_veux
 PRODUCT_SYSTEM_NAME := veux_global
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
